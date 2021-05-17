f23d5cef13b7d320922f41832e936e1b7f8d7254
Rafał Białek <bialekrafal94@gmail.com>
2021-05-18 00:22:40 +0200

    fix(): heroku db config

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,32 @@
 const config = require("../config/db.config");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
-  dialect: config.dialect,
-  operatorsAliases: false,
 
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+} else {
+  sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+
+    pool: {
+      max: config.pool.max,
+      min: config.pool.min,
+      acquire: config.pool.acquire,
+      idle: config.pool.idle,
+    },
+  });
+}
+
 const db = {};
 
 db.Sequelize = Sequelize;
